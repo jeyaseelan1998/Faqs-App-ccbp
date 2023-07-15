2d1e595b77b2a614b967ae56c509b156e9cb30c1
jeyaseelan Dell Pc <jeyaseelan.work@gmail.com>
2023-07-15 16:52:11 +0530

    code updated

--- src/components/FaqItem/index.js
+++ src/components/FaqItem/index.js
@@ -1,1 +1,37 @@
 // Write your code here.
+import { Component } from "react";
+import './index.css'
+
+class FaqItem extends Component {
+    state = {
+        showAnswer: false
+    }
+
+    onClickPlusButton = () => 
+        this.setState(prevState => ({showAnswer: !prevState.showAnswer}))
+
+    render() {
+        const { faqDetails: { questionText, answerText } } = this.props
+        const { showAnswer } = this.state
+        const iconImageUrl = showAnswer ? 'https://assets.ccbp.in/frontend/react-js/faqs-minus-icon-img.png' : 'https://assets.ccbp.in/frontend/react-js/faqs-plus-icon-img.png'
+        const altText = showAnswer ? 'minus' : 'plus'
+        return (
+            <li className='faq-container'>
+                <div className="question-btn-container">
+                    <h1 className='question'>{questionText}</h1>
+                    <button className="button" type="button" onClick={this.onClickPlusButton}>
+                        <img alt={altText} src={iconImageUrl} className='icon' />
+                    </button>
+                </div>
+                {showAnswer &&
+                    <>
+                        <hr className='separator' />
+                        <p className='answer'>{answerText}</p>
+                    </>
+                }
+            </li>
+        )
+    }
+}
+
+export default FaqItem
--- src/components/Faqs/index.js
+++ src/components/Faqs/index.js
@@ -1,1 +1,20 @@
-// Write your code here.
+import React from 'react'
+import './index.css'
+import FaqItem from '../FaqItem'
+
+const Faqs = ({faqsList}) => {
+  return (
+    <div className='app-container'>
+        <div className='faqs-container'>
+            <h1 className='main-heading'>Faqs</h1>
+            <ul className='faqs-lists'>
+                {
+                  faqsList.map(item => <FaqItem key={item.id} faqDetails={item}/>)
+                }
+            </ul>
+        </div>
+    </div>
+  )
+}
+
+export default Faqs
